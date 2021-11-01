eadf33e82b144b92f5cc592cba9298afc8be88a2
Jacek <jacek.kosy@outlook.de>
2021-11-01 20:34:35 +0100

    Finished task 4b

---
+++ Lab04/4b.js
@@ -0,0 +1,14 @@
+const fibonacci = function (n) {
+    let a = 0, b = 1, c, i
+    if (n === 0) {
+        return a
+    }
+    for (i = 2; i <= n; i++) {
+        c = a + b
+        a = b
+        b = c
+    }
+    return b
+}
+console.log(fibonacci(10))
+console.log(fibonacci(3))
